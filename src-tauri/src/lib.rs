3889c7b01c4c4fc5c7a1c1f6cefb84611c4d02fc
malashkadev <77845493+malashkadev@users.noreply.github.com>
2026-06-30 01:55:50 +0300

    feat: implement audio recorder component with cpal and hound

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 pub mod keyboard_hook;
+pub mod audio_recorder;
+
 
 #[tauri::command]
 fn greet(name: &str) -> String {
